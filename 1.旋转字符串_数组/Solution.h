367c554648328d0319ee1367dd68640e056d6fa8
rh_Jameson <huangruohai2010@gmail.com>
2014-12-10 21:47:52 +0800

    旋转字符串

--- 1.旋转字符串_数组/Solution.h
+++ 1.旋转字符串_数组/Solution.h
@@ -1,12 +1,21 @@
 /*************************************************************************
 	> File Name:        Solution for rotated Str
-	> Description:   
-                        (1)旋转字符串策略一：暴力移位（By一个字符）
+	> Description:      （1）问题描述：
+                            A.给定一字符串
+                            B.将串的前若干个元素移到串的尾部
+                            C."abcdefg",2  ==> "cdefgab"
+
+	> Conclusion:           
+                        （1）旋转字符串策略一：暴力移位（By一个字符）
                             A.A[0]存入临时变量tmp
                             B.数组其余元素放到数组后面
                             C.旋转几位，就执行几次A,B
 
-	> Conclusion:          
+                        （2）三步反转法
+                            A.要旋转的位置将串分为两段
+                            B.将这两段分别进行反转
+                            C.对整个字符串反转
+          
 	> Author:           rh_Jameson
 	> Created Time:     2014年12月07日 星期日 14时09分38秒
  ************************************************************************/
@@ -52,7 +61,7 @@ public:
     {
         int len = str.size();           //串长
         
-        /*cnt小于等于0,或大于等于串长,
+        /*cnt小于等于0,或大于等于串长时,
          *直接返回原字符串
          */
         if(cnt <= 0 || cnt >= len){
@@ -64,6 +73,7 @@ public:
 
         return str;
     }
+    //反转函数
     string reverse(int p, int q,string& str)
     {
         char tmp;
